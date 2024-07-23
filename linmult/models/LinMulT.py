Fix attention mask call for softmax mha

linmult/models/LinMulT.py
@@ -104,7 +104,7 @@ class LinMulT(nn.Module):
         elif self.add_cm_attention_back and self.n_modalities > 2:
             combined_dim = (self.n_modalities - 1) * (self.n_modalities - 1) * self.d_model
         else:
-            (self.n_modalities - 1) * self.n_modalities * self.d_model
+            combined_dim = (self.n_modalities - 1) * self.n_modalities * self.d_model
 
         # Optional: FFN + Residual
         if self.add_ffn_fusion:
@@ -242,7 +242,7 @@ if __name__ == "__main__":
     x_1 = torch.rand((batch_size, 450, 256))
     x_2 = torch.rand((batch_size, 1500, 25))
 
-    config_model = load_config('configs/2M_S_2O_V_GAP.yaml')
+    config_model = load_config('configs/2M_S_2O_V_GAP_mha.yaml')
     #config_model = load_config('configs/2M_S_2O_S_CMB.yaml')
 
     model = LinMulT(config=config_model)

linmult/models/transformer.py
@@ -91,6 +91,7 @@ class TransformerEncoderLayer(nn.Module):
 
         self.n_heads = config.get("n_heads", 8)
         self.d_model = config.get("d_model", 40)
+        self.attention_type = config.get("attention_type", "linear")
         self.attention = AttentionFactory.create_attention_layer(self.d_model, self.n_heads, config)
 
         # Feedforward network layers
@@ -142,7 +143,10 @@ class TransformerEncoderLayer(nn.Module):
                 key_mask = (x_k == 0).all(dim=2)
                 key_mask = key_mask.bool() # (B, T_2)
 
-            x_q, _ = self.attention(x_q, x_k, x_v, query_mask=query_mask, key_mask=key_mask) # returns (B, T_1, F) and (B, T_1, T_2)
+            if self.attention_type == "mha":
+                x_q, _ = self.attention(x_q, x_k, x_v)
+            else:
+                x_q, _ = self.attention(x_q, x_k, x_v, query_mask=query_mask, key_mask=key_mask) # returns (B, T_1, F) and (B, T_1, T_2)
         else: # self-attention
             if query_mask is not None and not query_mask.shape == x_q.shape[:2] and isinstance(query_mask, torch.BoolTensor):
                 raise ValueError(f"Expected query mask has (B, T) shape and BoolTensor type, got instead: {query_mask.shape} and {type(query_mask)}")
@@ -151,7 +155,10 @@ class TransformerEncoderLayer(nn.Module):
                 query_mask = (x_q == 0).all(dim=2)
                 query_mask = query_mask.bool() # (B, T_1)
 
-            x_q, _ = self.attention(x_q, x_q, x_q, query_mask=query_mask) # returns (B, T_1, F) and (B, T_1, T_1)
+            if self.attention_type == "mha":
+                x_q, _ = self.attention(x_q, x_q, x_q)
+            else:
+                x_q, _ = self.attention(x_q, x_q, x_q, query_mask=query_mask) # returns (B, T_1, F) and (B, T_1, T_1)
 
         x_q = F.dropout(x_q, p=self.dropout_residual, training=self.training)
         x_q = residual + x_q